982fcc27d7f925639d15c25b38827e5582f34d88
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-11 21:15:50 -0400

    Update fastpoly.jl

--- src/fastpoly.jl
+++ src/fastpoly.jl
@@ -96,3 +96,106 @@ https://github.com/JuliaLang/julia/pull/2987/commits/9c24795ac2918df7b8cba9bb129
  +                             -0.20601_07303_28265_443e2,
  +                              0.1e1)
  =#
+#=
+Base:math.jl
+
+# evaluate p[1] + x * (p[2] + x * (....)), i.e. a polynomial via Horner's rule
+macro horner(x, p...)
+    ex = esc(p[end])
+    for i = length(p)-1:-1:1
+        ex = :(muladd(t, $ex, $(esc(p[i]))))
+    end
+    Expr(:block, :(t = $(esc(x))), ex)
+end
+
+# Evaluate p[1] + z*p[2] + z^2*p[3] + ... + z^(n-1)*p[n].  This uses
+# Horner's method if z is real, but for complex z it uses a more
+# efficient algorithm described in Knuth, TAOCP vol. 2, section 4.6.4,
+# equation (3).
+macro evalpoly(z, p...)
+    a = :($(esc(p[end])))
+    b = :($(esc(p[end-1])))
+    as = []
+    for i = length(p)-2:-1:1
+        ai = Symbol("a", i)
+        push!(as, :($ai = $a))
+        a = :(muladd(r, $ai, $b))
+        b = :($(esc(p[i])) - s * $ai) # see issue #15985 on fused mul-subtract
+    end
+    ai = :a0
+    push!(as, :($ai = $a))
+    C = Expr(:block,
+             :(x = real(tt)),
+             :(y = imag(tt)),
+             :(r = x + x),
+             :(s = muladd(x, x, y*y)),
+             as...,
+             :(muladd($ai, tt, $b)))
+    R = Expr(:macrocall, Symbol("@horner"), :tt, map(esc, p)...)
+    :(let tt = $(esc(z))
+          isa(tt, Complex) ? $C : $R
+      end)
+end
+
+  http://stackoverflow.com/questions/28077057/julia-evalpoly-macro-with-varargs
+
+  from Erik Schnetter
+  c=[1,2,3,4]
+  @eval begin
+    function f()
+        @evalpoly 0.5 $(c...)
+    end
+end
+=#
+#=
+Investigation of different methods of fast polynomial evaluation
+Gavin S. Reynolds
+September 2010
+MSc in High Performance Computing
+The University of Edinburgh
+Year of Presentation: 2010
+
+The Optimised Brute Force function pre-calculates the value of x^2 and use it where possible in
+building up the powers for each monomial. For example, if a monomial contains x^7, instead of
+7 multiplications of x by x, the function would multiply x^2 by x^2three times then multiply it by x
+once. In this example, this would save three multiplications.
+
+function hornerform(x,coeffs)
+    n = length(coeffs)
+    z = coeffs[1]*x
+    for i in 2:(n-1)
+         z = (z + coeffs[i])*x
+    end
+    z + coeffs[n]
+end
+
+function bruteforce(x,coeffs)
+    n = length(coeffs)
+    monomials = zeros(typeof(x),n)
+    for i in 1:(n-1)
+        monomials[i] = coeffs[i] * (x^(n-i))
+    end
+    monomials[n] = coeffs[n]
+    sum(monomials)
+end
+
+function optforce(x,coeffs)
+    n = length(coeffs)
+    monomials = zeros(typeof(x),n)
+    x2 = x*x
+    for i in 1:(n-1)
+        monomials[i] = coeffs[i]
+        numsteps = (n-i) >> 1 # fld(n-i,2)
+        for k in 1:numsteps
+           monomials[k] *= x2
+        end
+        if isodd(n-i)
+            monomial[i] *= x
+        end
+    end
+    monomial[n] = coeffs[n]
+    sum(monomials)
+end
+
+end
+=#
