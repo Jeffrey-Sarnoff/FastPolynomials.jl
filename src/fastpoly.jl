e8d90310a95ddd1d987e63ad68060c28923c5b6e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-11 22:48:56 -0400

    Update fastpoly.jl

--- src/fastpoly.jl
+++ src/fastpoly.jl
@@ -1,5 +1,6 @@
 # from Polynomials.jl (Poly)
 
+
 """
 * `FastPoly{T<:Number}(a::Vector)`: Construct a polynomial from its coefficients, lowest order first. That is if `p=a_n x^n + ... + a_2 x^2 + a_1 x^1 + a_0`, we construct this through `Poly([a_0, a_1, ..., a_n])`.
 Example:
@@ -14,17 +15,22 @@ FastPoly([1,2,3], :s)       # FastPoly(1 + 2s + 3s^2)
 immutable FastPoly{T<:Number}
     a::Vector{T}
     var::Symbol
-    function Poly(a::Vector{T}, var)
+    
+    function FastPoly{S}(a::Vector{S}, var)
         # if a == [] we replace it with a = [0]
         if length(a) == 0
-            return new(zeros(T,1), symbol(var))
+            return new{S}(zeros(S,1), Symbol(var))
         else
             # determine the last nonzero element and truncate a accordingly
             a_last = max(1,findlast(a))
-            new(a[1:a_last], symbol(var))
+            new{S}(a[1:a_last], Symbol(var))
         end
     end
 end
+
+FastPoly{S}(a::Vector{S}, var) = FastPoly{S}(a,var)
+FastPoly{S}(a::Vector{S}) = FastPoly{S}(a,:x)
+
 #=
    via Julia High Performance by Avik Sengupta (pp 59-62)
 =#
