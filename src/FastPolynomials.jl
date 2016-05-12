1276ed836be453eaa1b6ee073879130d436b7e40
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-11 22:34:54 -0400

    Update FastPolynomials.jl

--- src/FastPolynomials.jl
+++ src/FastPolynomials.jl
@@ -1,10 +1,12 @@
 module FastPolynomials
 
+import Base.Math:@horner
+
 if isdefined(:Polynomials)
    import Polynomials:polyval
 end
 
-export FastPoly, polyval
+export FastPoly, polyval, @hornerFMA
 
 include("fastpoly.jl")
 
